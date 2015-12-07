8f1ad114e1a5f9c4fe817de2d17d91336239b57e
Michael Rodler <michael@michaelrodler.at>
2015-12-07 16:40:33 +0100

    Added list of openssl headers for tracer generation

--- tracers/generate.sh
+++ tracers/generate.sh
@@ -3,7 +3,7 @@
 listdir="$(pwd)/"
 
 pushd ../tracergen/
-for list in cstd_headers.txt posix_headers.txt; do
+for list in cstd_headers.txt posix_headers.txt openssl_headers.txt; do
     out="$(basename -s '_headers.txt' "$list").c"
     echo "Generating $out from $list"
     ./lltaptracergen -o "$listdir/$out" --from-lists "$listdir/$list"
